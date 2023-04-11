26345cf65fd33d0f00869e6e79ae88bb5f85ff4a
sergiohj93 <sergiohernandezjimenez1993@gmail.com>
2023-04-11 21:42:58 +0200

    Simple model: Train + Reproduce

---
+++ utils.py
@@ -0,0 +1,55 @@
+import scipy
+import numpy as np
+
+def cast_list_as_strings(mylist):
+    """
+    return a list of strings
+    """
+    #assert isinstance(mylist, list), f"the input mylist should be a list it is {type(mylist)}"
+    mylist_of_strings = []
+    for x in mylist:
+        mylist_of_strings.append(str(x))
+
+    return mylist_of_strings
+
+
+def get_features_from_df(df, count_vectorizer):
+    """
+    returns a sparse matrix containing the features build by the count vectorizer.
+    Each row should contain features from question1 and question2.
+    """
+    q1_casted =  cast_list_as_strings(list(df["question1"]))
+    q2_casted =  cast_list_as_strings(list(df["question2"]))
+    
+    ############### Begin exercise ###################
+    # what is kaggle                  q1
+    # What is the kaggle platform     q2
+    X_q1 = count_vectorizer.transform(q1_casted)
+    X_q2 = count_vectorizer.transform(q2_casted)
+
+    X_q1q2 = scipy.sparse.hstack((X_q1,X_q2))
+    ############### End exercise ###################
+
+    return X_q1q2
+
+
+def get_mistakes(clf, X_q1q2, y):
+
+    ############### Begin exercise ###################
+    predictions = clf.predict(X_q1q2)
+    incorrect_predictions = predictions != y 
+    incorrect_indices,  = np.where(incorrect_predictions)
+    
+    ############### End exercise ###################
+    
+    if np.sum(incorrect_predictions)==0:
+        print("no mistakes in this df")
+    else:
+        return incorrect_indices, predictions
+    
+
+def print_mistake_k(k, train_df, mistake_indices, predictions):
+    print(train_df.iloc[mistake_indices[k]].question1)
+    print(train_df.iloc[mistake_indices[k]].question2)
+    print("true class:", train_df.iloc[mistake_indices[k]].is_duplicate)
+    print("prediction:", predictions[mistake_indices[k]])
